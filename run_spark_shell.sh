d00c0077b62c74391d12a5a611b4bb3f3c795184
TjWallas <tjwallas@cryptolab.net>
2016-06-30 18:08:31 +0200

    Run spark script takes selected algorithm as input

--- run_spark_shell.sh
+++ run_spark_shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D$1,$2,$3" --master local[4] --jars /opt/spark-1.6.2-bin-hadoop2.6/lib/graphframes-0.1.0-spark1.6.jar
+spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D$1,$2,$3,$4" --master local[4] --jars /opt/spark-1.6.2-bin-hadoop2.6/lib/graphframes-0.1.0-spark1.6.jar
