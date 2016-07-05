33100735f9de33bbf26a2cd6729ee358cc537a22
TjWallas <tjwallas@cryptolab.net>
2016-07-05 15:40:16 +0200

    Adds some documentation to the run spark shellscript

--- run_spark_shell.sh
+++ run_spark_shell.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+#
+# Arguments:
+# 0: Input vertices file path
+# 1: Input edges file path
+# 2: Output file path
+# 3: Selected Algorithm ID
+
 spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D$1,$2,$3,$4" --master local[4] --jars /opt/spark-1.6.2-bin-hadoop2.6/lib/graphframes-0.1.0-spark1.6.jar
