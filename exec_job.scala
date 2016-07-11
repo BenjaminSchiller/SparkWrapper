259b69c72bdbae9e656548e57d1054296d7193a5
TjWallas <tjwallas@cryptolab.net>
2016-07-11 10:26:52 +0200

    Uncomments shortest paths counting

--- exec_job.scala
+++ exec_job.scala
@@ -69,11 +69,11 @@ if (selectedAlgorithm == "1") {
 
 } else if (selectedAlgorithm == "2") {
 	println("Running vertex degrees...");
-	time { result=graphFrame.degrees }; 
-	// TODO: subgraphing is supported via graphx
+	time { result=graphFrame.degrees };
+ 
 } else if (selectedAlgorithm == "4") {
 	println("Running shortest paths...");
-	// time { graphFrame.shortestPaths.landmarks(Array(srcVertextShortestPaths)).run }; 
+	time { graphFrame.shortestPaths.landmarks(Array(srcVertextShortestPaths)).run }; 
 
 } else if (selectedAlgorithm == "5") {
 	println("Running triangle count...");
