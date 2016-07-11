56c2f8cd34e726a72eb3fea91222b471dc14e744
TjWallas <tjwallas@cryptolab.net>
2016-07-11 10:24:30 +0200

    Spark takes number of executors as argument

--- run_spark_shell.sh
+++ run_spark_shell.sh
@@ -6,5 +6,11 @@
 # 2: Output file path
 # 3: Selected Algorithm ID
 # 4: Src vertex to compute shortest paths
+# 5: Number of worker threads -- Set this to the number of *LOGICAL* cores
+#
+# For extra spark config, see: https://spark.apache.org/docs/latest/configuration.html
+# --master local[*] Run spark with as many worker threads as logical cores on the machine.
+#
+#
 
-spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D$1,$2,$3,$4,$5" --master local[4] --jars /opt/spark-1.6.2-bin-hadoop2.6/lib/graphframes-0.1.0-spark1.6.jar
+spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D$1,$2,$3,$4,$5" --master local[$6] --jars /opt/spark-1.6.2-bin-hadoop2.6/lib/graphframes-0.1.0-spark1.6.jar
