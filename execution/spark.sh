07ee95883e279fdce6dcfa3c05340176881bd1e0
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2016-08-02 08:35:15 +0200

    use paths from config in wrapper and add total measured time

--- execution/spark.sh
+++ execution/spark.sh
@@ -75,27 +75,31 @@ for s in $(seq 0 $((states-1))); do
 			err="${logDir}/${run}-${s}--${vertexId}${errSuffix}"
 			output="${outputDir}/${run}-${s}--${vertexId}${outputSuffix}"
 			# spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D${datasetPathV},${datasetPathE},${output},${metricId},${vertexId}" --master local[${workers}] --jars $jarPath > >(tee $log) 2> >(tee $err >&2)
-			./submit_spark_job.sh ${datasetPathV} ${datasetPathE} ${output} ${metricId} ${vertexId} ${workers} > >(tee $log) 2> >(tee $err >&2)
+			# ./submit_spark_job.sh ${datasetPathV} ${datasetPathE} ${output} ${metricId} ${vertexId} ${workers} > >(tee $log) 2> >(tee $err >&2)
+			$sparkSubmitPath --conf spark.driver.extraJavaOptions="-D${datasetPathV},${datasetPathE},${output},${metricId},${vertexId}" --master local[${workers}] --jars $sparkJarPath --class DGARunner $scalaRunnerPath > >(tee $log) 2> >(tee $err >&2)
 		done
 	else
 		log="${logDir}/${run}-${s}${logSuffix}"
 		err="${logDir}/${run}-${s}${errSuffix}"
 		output="${outputDir}/${run}-${s}${outputSuffix}"
 		# spark-shell -i exec_job.scala --conf spark.driver.extraJavaOptions="-D${datasetPathV},${datasetPathE},${output},${metricId},${metricArguments}" --master local[${workers}] --jars $jarPath > >(tee $log) 2> >(tee $err >&2)
-		./submit_spark_job.sh ${datasetPathV} ${datasetPathE} ${output} ${metricId} ${metricArguments} ${workers} > >(tee $log) 2> >(tee $err >&2)
+		# ./submit_spark_job.sh ${datasetPathV} ${datasetPathE} ${output} ${metricId} ${metricArguments} ${workers} > >(tee $log) 2> >(tee $err >&2)
+		$sparkSubmitPath --conf spark.driver.extraJavaOptions="-D${datasetPathV},${datasetPathE},${output},${metricId},${metricArguments}" --master local[${workers}] --jars $sparkJarPath --class DGARunner $scalaRunnerPath > >(tee $log) 2> >(tee $err >&2)
 	fi
 	total_end=$(printTime)
 	duration=$((${total_end} - ${total_start}))
 	ioTime=$(grep "Elapsed time:" $log | head -n1 | sed 's/Elapsed time: //g' | sed 's/ns//g')
 	executionTime=$(grep "Elapsed time:" $log | tail -n1 | sed 's/Elapsed time: //g' | sed 's/ns//g')
-	echo "$s	$duration	$ioTime	$executionTime" >> $runtimes
-	echo "$s	$duration	$ioTime	$executionTime"
+	measuredTime=$(($ioTime+$executionTime))
+	echo "$s	$duration	$measuredTime	$ioTime	$executionTime" >> $runtimes
+	echo "$s	$duration	$measuredTime	$ioTime	$executionTime"
 done
 sumA=$(awk '{ sum += $2; } END { print sum; }' "$runtimes")
 sumB=$(awk '{ sum += $3; } END { print sum; }' "$runtimes")
 sumC=$(awk '{ sum += $4; } END { print sum; }' "$runtimes")
-echo "TOTAL	$sumA	$sumB	$sumC" >> $runtimes
-echo "TOTAL	$sumA	$sumB	$sumC"
+sumD=$(awk '{ sum += $5; } END { print sum; }' "$runtimes")
+echo "TOTAL	$sumA	$sumB	$sumC	$sumD" >> $runtimes
+echo "TOTAL	$sumA	$sumB	$sumC	$sumD"
 
 ./stop-master.sh
 
